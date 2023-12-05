d98fe804394327fe3989a0290467c7a144cf116a
stellanechi <stellanechi65@gmail.com>
2023-12-05 14:06:32 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <task v-bind:tasks="tasks"></task>
+  </div>
+</template>
+
+<script>
+import Task from "./components/Task";
+
+export default {
+  name: "App",
+  components: {
+    Task,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Learn Vue router",
+          completed: true,
+        },
+        {
+          id: 2,
+          title: "Send cold Email",
+          completed: true,
+        },
+        {
+          id: 3,
+          title: "Lunch",
+          completed: false,
+        },
+        {
+          id: 4,
+          title: "Go shopping",
+          completed: false,
+        },
+        {
+          id: 5,
+          title: "Make Dinner",
+          completed: false,
+        },
+      ],
+    };
+  },
+};
+</script>
